55c4af7d3689abe31a2ba89b6b4c6b9ec3941499
Lei <kanlei@live.com>
2016-03-30 23:09:52 +0800

    增加自定义 View Demo

---
+++ Controls/NotifyBadge.cs
@@ -0,0 +1,256 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using System.Drawing;
+using CoreGraphics;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// 用于显示消息提示数量
+    /// </summary>
+    [Register("NotifyBadge")]
+    public class NotifyBadge:UIView
+    {
+        const float M_PI = (float)Math.PI;
+        CGSize contentSize = new CGSize(20, 20);
+
+        public NotifyBadge(IntPtr handle)
+            : base(handle)
+        {
+            Initialize();
+        }
+
+        public NotifyBadge()
+        {
+            Initialize();
+        }
+
+        private string count;
+
+        /// <summary>
+        /// 提示消息数
+        /// </summary>
+        public string Count
+        {
+            get{ return count; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+
+                count = value;
+                AutoBadgeSizeWithString(count);
+            }
+        }
+
+        /// <summary>
+        /// 字体
+        /// </summary>
+        public UIFont Font{ get; set; }
+
+        /// <summary>
+        /// 文本颜色
+        /// </summary>
+        public UIColor TextColor{ get; set; }
+
+        /// <summary>
+        /// 填充颜色
+        /// </summary>
+        public UIColor InsetColor{ get; set; }
+
+        /// <summary>
+        /// 边框颜色
+        /// </summary>
+        public UIColor FrameColor{ get; set; }
+
+        /// <summary>
+        /// 边框是否可见
+        /// </summary>
+        public bool FrameVisible{ get; set; }
+
+        /// <summary>
+        /// 是否呈现闪亮✨     的效果
+        /// </summary>
+        public bool Shining{ get; set; }
+
+        /// <summary>
+        /// 圆角
+        /// </summary>
+        public float CornerRoundness{ get; set; }
+
+        /// <summary>
+        /// 缩放比例
+        /// </summary>
+        public float ScaleFactor{ get; set; }
+
+        private void Initialize()
+        {
+            ContentScaleFactor = UIScreen.MainScreen.Scale;
+            BackgroundColor = UIColor.Clear;
+
+            this.TextColor = UIColor.White;
+            this.Font = UIFont.SystemFontOfSize(12);
+            this.FrameVisible = false;
+            this.FrameColor = UIColor.White;
+            this.InsetColor = UIColor.Red;
+            this.CornerRoundness = 0.4f;
+            this.ScaleFactor = 0.95f;
+            this.Shining = false;
+        }
+
+        public void AutoBadgeSizeWithString(string badgeString)
+        {
+            CGSize retValue;
+            nfloat rectWidth, rectHeight;
+            CGSize stringSize = new NSString(badgeString).GetSizeUsingAttributes(new UIStringAttributes{ Font = Font });
+            nfloat flexSpace;
+            if (badgeString.Length >= 2)
+            {
+                flexSpace = badgeString.Length;
+                rectWidth = 15 + (stringSize.Width + flexSpace);
+                rectHeight = 20;
+                retValue = new CGSize(rectWidth * ScaleFactor, rectHeight * ScaleFactor);
+            }
+            else
+            {
+                retValue = new SizeF(20 * ScaleFactor, 20 * ScaleFactor);
+            }
+            contentSize = new CGSize(retValue.Width, retValue.Height);
+            InvalidateIntrinsicContentSize();
+            SetNeedsDisplay();
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                DrawRoundedRect(rect, context);
+                if (Shining)
+                {
+                    DrawShine(rect, context);
+                }
+                if (FrameVisible)
+                {
+                    DrawFrame(rect, context);
+                }
+                if (!string.IsNullOrWhiteSpace(Count))
+                {
+                    float sizeOfFont = 13.5f * ScaleFactor;
+                    if (Count.Length < 2)
+                    {
+                        sizeOfFont += sizeOfFont * 0.20f;
+                    }
+
+                    var nsText = new NSString(Count);
+                    var textFont = Font.WithSize(sizeOfFont);
+
+                    var textSize = nsText.GetSizeUsingAttributes(new UIStringAttributes{ Font = textFont });
+                    nsText.DrawString(new CGPoint((rect.Width / 2) - (textSize.Width / 2), 
+                            rect.Height / 2 - textSize.Height / 2),
+                        new UIStringAttributes{ Font = textFont, ForegroundColor = TextColor });
+                }
+            }
+        }
+
+        /// <summary>
+        /// 绘制圆角矩形
+        /// </summary>
+        void DrawRoundedRect(CGRect rect, CGContext context)
+        {
+            context.SaveState();
+            nfloat radius = rect.GetMaxY() * CornerRoundness;
+            nfloat puffer = rect.GetMaxY() * 0.10f;
+            nfloat maxX = rect.GetMaxX() - puffer;
+            nfloat maxY = rect.GetMaxY() - puffer;
+            nfloat minX = rect.GetMinX() + puffer;
+            nfloat minY = rect.GetMinY() + puffer;
+
+            context.BeginPath();
+            context.SetFillColor(InsetColor.CGColor);
+            context.AddArc(maxX - radius, minY + radius, radius, M_PI + (M_PI / 2f), 0f, false);
+            context.AddArc(maxX - radius, maxY - radius, radius, 0, M_PI / 2f, false);
+            context.AddArc(minX + radius, maxY - radius, radius, M_PI / 2f, M_PI, false);
+            context.AddArc(minX + radius, minY + radius, radius, M_PI, M_PI + (M_PI / 2f), false);
+            //            context.SetShadowWithColor (new SizeF (1, 1), 3, UIColor.Black.CGColor);
+            context.FillPath();
+            context.RestoreState();
+        }
+
+        /// <summary>
+        /// 绘制闪亮的效果
+        /// </summary>
+        void DrawShine(CGRect rect, CGContext context)
+        {
+            context.SaveState();
+            nfloat radius = rect.GetMaxY() * CornerRoundness;
+            nfloat puffer = rect.GetMaxY() * 0.10f;
+            nfloat maxX = rect.GetMaxX() - puffer;
+            nfloat maxY = rect.GetMaxY() - puffer;
+            nfloat minX = rect.GetMinX() + puffer;
+            nfloat minY = rect.GetMinY() + puffer;
+
+            context.BeginPath();
+            context.AddArc(maxX - radius, minY + radius, radius, M_PI + (M_PI / 2f), 0f, false);
+            context.AddArc(maxX - radius, maxY - radius, radius, 0, M_PI / 2f, false);
+            context.AddArc(minX + radius, maxY - radius, radius, M_PI / 2f, M_PI, false);
+            context.AddArc(minX + radius, minY + radius, radius, M_PI, M_PI + (M_PI / 2f), false);
+            context.Clip();
+
+            var locations = new nfloat[]{ 0f, 0.4f };
+            var components = new nfloat[]{ 0.92f, 0.92f, 0.92f, 1.0f, 0.82f, 0.82f, 0.82f, 0.4f };
+            using (var cspace = CGColorSpace.CreateDeviceRGB())
+            using (var gradient = new CGGradient(cspace, components, locations))
+            {
+
+                var sPoint = new CGPoint(0, 0);
+                var ePoint = new CGPoint(0, maxY);
+                context.DrawLinearGradient(gradient, sPoint, ePoint, (CGGradientDrawingOptions)0);
+            }
+            context.RestoreState();
+        }
+
+        /// <summary>
+        /// 绘制边框
+        /// </summary>
+        void DrawFrame(CGRect rect, CGContext context)
+        {
+            nfloat radius = rect.GetMaxY() * CornerRoundness;
+            nfloat puffer = rect.GetMaxY() * 0.10f;
+            nfloat maxX = rect.GetMaxX() - puffer;
+            nfloat maxY = rect.GetMaxY() - puffer;
+            nfloat minX = rect.GetMinX() + puffer;
+            nfloat minY = rect.GetMinY() + puffer;
+            context.BeginPath();
+            nfloat lineSize = 2;
+            if (ScaleFactor > 1)
+            {
+                lineSize += ScaleFactor * 0.25f;
+            }
+            context.SetLineWidth(lineSize);
+            context.SetStrokeColor(FrameColor.CGColor);
+
+            context.AddArc(maxX - radius, minY + radius, radius, M_PI + (M_PI / 2f), 0f, false);
+            context.AddArc(maxX - radius, maxY - radius, radius, 0, M_PI / 2f, false);
+            context.AddArc(minX + radius, maxY - radius, radius, M_PI / 2f, M_PI, false);
+            context.AddArc(minX + radius, minY + radius, radius, M_PI, M_PI + (M_PI / 2f), false);
+            context.ClosePath();
+            context.StrokePath();
+        }
+
+        /// <summary>
+        /// 不阻塞点击事件
+        /// </summary>
+        public override bool PointInside(CGPoint point, UIEvent uievent) => false;
+
+        /// <summary>
+        /// 只有使用 AutoLayout 布局时才会调用, AutoLayout 决定位置, 该值决定其大小
+        /// </summary>
+        public override CGSize IntrinsicContentSize => contentSize;
+    }
+}
---
+++ CustomView.cs
@@ -0,0 +1,102 @@
+﻿using UIKit;
+
+
+// Custom Control with Autolayout: https://www.objc.io/issues/3-views/advanced-auto-layout-toolbox/
+// Alignment Rect: http://www.informit.com/articles/article.aspx?p=2041295&seqNum=9
+
+namespace DemoApp
+{
+    public class CustomView:UIView
+    {
+        public CustomView()
+        {
+        }
+
+        // 计算过程
+        // Down-Top 从子视图到父视图的过程
+        // 任何对约束本身的更改，都会自动触发该过程
+        // 显示地触发该过程通过调用 SetNeedsUpdateConstraints()
+        public override void UpdateConstraints()
+        {
+            // 为 subview 增加自定义约束
+            // 避免重复添加约束
+
+            base.UpdateConstraints();  // must call base
+        }
+
+
+        // 将 UpdateConstraints 的解决方案应用到视图的 center 和 bound
+        // Top-Down 从父视图到子视图的过程
+        // 显示地触发该过程通过调用 SetNeedsLayout()
+        // 立即触发该过程通过调用 LayoutIfNeed()
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            // 在 View 布局之后，根据需要重新定位
+
+            // 如果使用的控件要支持多行文本，则在这里设置 referredMaxLayoutWidth 用于计算固有大小，并重新调用 base.LayoutSubviews();
+            // 或者在控件的子类的 LayoutSubViews 方法中 this.PreferredMaxLayoutWidth = this.Frame.Size.Width;base.LayoutSubviews();
+            // 此时无需先调用 base，因为执行子类的 LayoutSubviews 方法时，Frame 已经确定，LayoutSubviews 自顶向下布局
+        }
+
+
+        // 显示过程
+        // Top-Down 从父视图到子视图的过程
+        // 不依赖于是否采用 AutoLayout
+        public override void Draw(CoreGraphics.CGRect rect)
+        {
+            base.Draw(rect);
+
+
+        }
+
+
+        // 固有尺寸，自动转化为约束
+        // 任何对固有尺寸有影响的改变都应该调用 InvalidateIntrinsicContentSize();
+        public override CoreGraphics.CGSize IntrinsicContentSize
+        {
+            get
+            {
+                return base.IntrinsicContentSize;
+            }
+        }
+
+        #region 以下两个方法不能在子类中重写，而是在实例化 View 时，显式地指定
+        // 压缩阻力，IntrinsicContentSize 存在的条件下起作用
+        public override float ContentCompressionResistancePriority(UILayoutConstraintAxis axis)
+        {
+            return base.ContentCompressionResistancePriority(axis);
+        }
+
+
+        // 吸附阻力，IntrinsicContentSize 存在的条件下起作用
+        public override float ContentHuggingPriority(UILayoutConstraintAxis axis)
+        {
+            return base.ContentHuggingPriority(axis);
+        }
+        #endregion
+
+        // 对齐矩形
+        // Auto Layout does not operate on views’ frame, but on their alignment rect.
+        // https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIView_Class/#//apple_ref/occ/instm/UIView/alignmentRectInsets
+        public override UIEdgeInsets AlignmentRectInsets
+        {
+            get
+            {
+                return base.AlignmentRectInsets;
+            }
+        }
+
+
+        public override UIView ViewForBaselineLayout
+        {
+            get
+            {
+                return base.ViewForBaselineLayout;
+            }
+        }
+
+    }
+}
+
