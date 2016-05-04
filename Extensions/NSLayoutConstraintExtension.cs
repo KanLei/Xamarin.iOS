81b9bf210087ae273281e02ed2d45f036ea3370a
kanleilei <kanleilei@sodao.com>
2016-05-04 14:56:56 +0800

    add AddConstraint extension method

---
+++ Extensions/NSLayoutConstraintExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UIKit;
+
+public static class NSLayoutConstraintExtension
+{
+    private static NSLayoutConstraint[] VisualFormat(string format, NSLayoutFormatOptions formatOptions, params object[] viewsAndMetrics)
+    {
+        if (format == null)
+        {
+            throw new ArgumentNullException(nameof(format));
+        }
+
+        if (viewsAndMetrics == null)
+        {
+            throw new ArgumentNullException(nameof(viewsAndMetrics));
+        }
+
+        return NSLayoutConstraint.FromVisualFormat(format, formatOptions, viewsAndMetrics);
+    }
+
+    public static void AddConstraints(this UIView view, string visualFormat, NSLayoutFormatOptions formatOptions, params object[] viewsAndMetrics)
+    {
+        view.AddConstraints(VisualFormat(visualFormat, formatOptions, viewsAndMetrics));
+    }
+}
