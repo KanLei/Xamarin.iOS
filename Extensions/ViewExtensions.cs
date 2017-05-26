d380dc3fff07bf42bfe2240f83a3a6d1465ab4f1
Lei <kanlei@live.com>
2017-05-26 13:49:23 +0800

    add ViewExtensions

---
+++ Extensions/ViewExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+using CoreAnimation;
+
+public static class ViewExtensions
+{
+	public static void Round (this UIView view, UIRectCorner corners, nfloat radius)
+	{
+		var bezierPath = UIBezierPath.FromRoundedRect (view.Bounds,
+		                                               corners,
+		                                               new CGSize (radius, radius));
+		var shapeLayer = new CAShapeLayer ();
+		shapeLayer.Frame = view.Bounds;
+		shapeLayer.Path = bezierPath.CGPath;
+		view.Layer.Mask = shapeLayer;
+	}
+}
